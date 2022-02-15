f0f74ea7db8a79999b15a4e79f389ac77853dbc2
samsausville <sps222@lehigh.edu>
2022-02-15 15:06:22 -0500

    Added comments and got rid of default stuff

--- socsem_flutter/lib/pages/reading_timer_page.dart
+++ socsem_flutter/lib/pages/reading_timer_page.dart
@@ -9,32 +9,36 @@ class ReadingTimerPage extends StatefulWidget {
 }
 
 class _ReadingTimerPageState extends State<ReadingTimerPage> {
-  Duration duration = Duration();
+  Duration duration = const Duration();
   Timer? timer;
 
   @override
   void initState() {
     super.initState();
 
+    // Reset on Page Load
     reset();
   }
 
   void reset() {
-    setState(() => duration = Duration());
+    setState(() => duration = const Duration());
   }
 
   void stopTimer({bool resets = true}) {
+    // Stops and if Resets is true reset the timer
     if (resets) {
       reset();
     }
 
+    // Stop the timer from continuing
     setState(() {
       timer?.cancel();
     });
   }
 
+  // Increment Time
   void addTime() {
-    final addSeconds = 1;
+    const addSeconds = 1;
 
     setState(() {
       final seconds = duration.inSeconds + addSeconds;
@@ -43,6 +47,7 @@ class _ReadingTimerPageState extends State<ReadingTimerPage> {
   }
 
   void startTimer({bool resets = true}) {
+    // If resets is true, reset the timer
     if (resets) {
       reset();
     }
@@ -52,6 +57,7 @@ class _ReadingTimerPageState extends State<ReadingTimerPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Column(children: [
+          // Sized Box moves items up and down
           const SizedBox(height: 200),
           buildTime(),
           const SizedBox(height: 80),
@@ -79,11 +85,11 @@ class _ReadingTimerPageState extends State<ReadingTimerPage> {
   Widget buildTimeCard({required String time, required String header}) =>
       Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Container(
-            padding: EdgeInsets.all(8),
+            padding: const EdgeInsets.all(8),
             decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(20)),
             child: Text(time,
-                style: TextStyle(
+                style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     color: Colors.black,
                     fontSize: 72))),
