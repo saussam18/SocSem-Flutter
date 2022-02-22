5c0d4a0a95c68920e4d6b566a3e082e5077ae2b8
samsausville <sps222@lehigh.edu>
2022-02-22 16:08:39 -0500

    Added firebase to ios

--- socsem_flutter/lib/main.dart
+++ socsem_flutter/lib/main.dart
@@ -1,20 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:socsem_flutter/pages/reading_timer_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SocSem',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: ReadingTimerPage(),
     );
---
+++ socsem_flutter/lib/widgets/sign_in_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+/*
+class SignInButton extends StatefulWidget {
+  final FaIcon faIcon;
+  final LoginType loginType;
+
+  SignInButton({Key? key, required this.faIcon, required this.loginType})
+      : super(key: key);
+
+  @override
+  _SignInButtonState createState() => _SignInButtonState();
+} */
